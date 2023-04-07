5cfdb02dbd0914ac196afb38c71cbe63bbcccc9c
Alan Chan <alloppi@gmail.com>
2023-04-07 18:49:10 +0100

    Add project files.

---
+++ ATMApp/App/ATMApp.cs
@@ -0,0 +1,73 @@
+﻿using ATMApp.Domain.Entities;
+using ATMApp.Domain.Interfaces;
+using ATMApp.UI;
+
+namespace ATMApp
+{
+    public class ATMApp : IUserLogin
+    { 
+        private List<UserAccount> userAcccountList;
+        private UserAccount selectedAccount;
+
+        public void InitializeData()
+        {
+            userAcccountList = new List<UserAccount>
+            {
+                new UserAccount { Id = 1, FullName = "Alan Chan", AccountNumber = 123456, CardNumber = 321321, CardPin = 123123, AccountBalance = 10000.00m, IsLocked = false },
+                new UserAccount { Id = 2, FullName = "Bob Lee", AccountNumber = 456789, CardNumber = 111555, CardPin = 123789, AccountBalance = 30000.00m, IsLocked = false },
+                new UserAccount { Id = 3, FullName = "Jackie Lo", AccountNumber = 654321, CardNumber = 222444, CardPin = 123456, AccountBalance = 40000.00m, IsLocked = true },
+                new UserAccount { Id = 4, FullName = "Fiona Chung", AccountNumber = 123457, CardNumber = 321322, CardPin = 123457, AccountBalance = 1000.00m, IsLocked = false },
+            };
+        }
+
+        public void CheckUserCardNumAndPassword()
+        {
+            bool isCorrrectLogin = false;
+            while (isCorrrectLogin == false)
+            {
+                UserAccount inputAccount = AppScreen.UserLoginForm();
+                AppScreen.LoginProgress();
+                foreach (UserAccount account in userAcccountList)
+                {
+                    selectedAccount = account;
+                    if (inputAccount.CardNumber.Equals(selectedAccount.CardNumber))
+                    {
+                        selectedAccount.TotalLogin++;
+
+                        if (inputAccount.CardPin.Equals(selectedAccount.CardPin))
+                        {
+                            selectedAccount = account;
+
+                            if (selectedAccount.IsLocked || selectedAccount.TotalLogin > 3) 
+                            {
+                                AppScreen.PrintLockScreen();
+                            }
+                            else
+                            {
+                                selectedAccount.TotalLogin = 0;
+                                isCorrrectLogin = true;
+                                break;
+                            }
+                        }
+                    }
+                }
+                if (isCorrrectLogin == false)
+                {
+                    Utility.PrintMessage("\nInvalid card number or PIN.", false);
+                    selectedAccount.TotalLogin++;
+                    selectedAccount.IsLocked = (selectedAccount.TotalLogin == 3);
+                    if (selectedAccount.IsLocked)
+                    {
+                        AppScreen.PrintLockScreen();
+                    }
+                }
+                Console.Clear();
+            }
+        }
+
+        public void Welcome()
+        {
+            Console.WriteLine($"Welecome back, {selectedAccount.FullName}");
+        }
+    }
+}
---
+++ ATMApp/UI/AppScreen.cs
@@ -0,0 +1,55 @@
+﻿using ATMApp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATMApp.UI
+{
+    public static class AppScreen
+    {
+        internal static void Welcome()
+        {
+            // Clear the console screen
+            Console.Clear();
+
+            // Set the title of the console window
+            Console.Title = "My ATM App";
+
+            // Set the text color or foreground color to white
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.WriteLine("\n\n---------------Welcome to My ATM App---------------\n\n");
+
+            // prompt the user to insert ATM card
+            Console.WriteLine("Please insert your ATM card");
+            Console.WriteLine("Note: Actual ATM machine will accept and validate" +
+                " a physical ATM card, read the card number and validate it.");
+            Utility.PressEnterToContinue();
+        }
+
+        internal static UserAccount UserLoginForm()
+        {
+            UserAccount tempUserAccount = new UserAccount();
+            tempUserAccount.CardNumber = Validator.Convert<long>("Your ATM card number");
+            tempUserAccount.CardPin = Convert.ToInt32(Utility.GetSecretInput("Enter your card PIN"));
+            return tempUserAccount;
+        }
+
+        internal static void LoginProgress() 
+        {
+            Console.WriteLine("\nChecking card number and PIN");
+            Utility.PrintDotAnimation();
+        }
+
+        internal static void PrintLockScreen() 
+        {
+            Console.Clear();
+            Utility.PrintMessage("Your account is locked. Please go to the nearest branch" +
+                " to unlock your account.", true);
+            Utility.PressEnterToContinue();
+            Environment.Exit(1);
+        }
+    }
+}
